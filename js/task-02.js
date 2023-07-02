55955c901277e8995413a30ed34252a44a5b325d
ZhukRuslan <ruslan.zhuk.rz@gmail.com>
2023-07-02 22:22:20 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,11 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const listIngredients = ingredients.map(ingredient => {
+  const listIngredient = document.createElement("li");
+  listIngredient.classList.add("item");
+  listIngredient.textContent = ingredient;
+  return listIngredient;
+});
+const ulIngredients = document.querySelector("ul");
+ulIngredients.append(...listIngredients);
